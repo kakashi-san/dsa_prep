d3ee4262c5ca7af7f6a3ea5dbed1a2b55eadd844
kakashi-san <siddheshpednekar94@gmail.com>
2023-09-02 17:37:15 +0530

    added prepend method to LinkedList class

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -102,8 +102,24 @@ class LinkedList:
         if self.length == 0:
             return None
         
+        temp = self.head
         if self.length == 1:
-            
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return temp
+        
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        
+        self.tail = pre
+        self.tail.next = None
+        self.length -=1
+        return temp
+
+
 
 """
 [CONSTRUCTOR VALIDATION]
@@ -157,4 +173,67 @@ my_linked_list.print_list()
     1
     2
     
+"""
+
+##########################################################   
+##   Test code below will print output to "User logs"   ##
+##########################################################
+"""
+def check(expect, actual, message):
+    print(message)
+    print("EXPECTED:", expect)
+    print("RETURNED:", actual)
+    print("PASS" if expect == actual else "FAIL", "\n")
+
+print("\n----- Test: Pop on linked list with one node -----\n")
+linked_list = LinkedList(1)
+linked_list.print_list()
+popped_node = linked_list.pop()
+check(1, popped_node.value, "Value of popped node:")
+check(None, linked_list.head, "Head of linked list:")
+check(None, linked_list.tail, "Tail of linked list:")
+check(0, linked_list.length, "Length of linked list:")
+
+print("\n----- Test: Pop on linked list with multiple nodes -----\n")
+linked_list = LinkedList(1)
+linked_list.append(2)
+linked_list.append(3)
+linked_list.print_list()
+popped_node = linked_list.pop()
+check(3, popped_node.value, "Value of popped node:")
+check(1, linked_list.head.value, "Head of linked list:")
+check(2, linked_list.tail.value, "Tail of linked list:")
+check(2, linked_list.length, "Length of linked list:")
+
+print("\n----- Test: Pop on empty linked list -----\n")
+linked_list = LinkedList(1)
+linked_list.head = None
+linked_list.tail = None
+linked_list.length = 0
+popped_node = linked_list.pop()
+check(None, popped_node, "Popped node from empty linked list:")
+check(None, linked_list.head, "Head of linked list:")
+check(None, linked_list.tail, "Tail of linked list:")
+check(0, linked_list.length, "Length of linked list:")
+
+print("\n----- Test: Pop all -----\n")
+linked_list = LinkedList(1)
+linked_list.append(2)
+linked_list.print_list()
+popped_node = linked_list.pop()
+check(2, popped_node.value, "Value of popped node (first pop):")
+check(1, linked_list.head.value, "Head of linked list (after first pop):")
+check(1, linked_list.tail.value, "Tail of linked list (after first pop):")
+check(1, linked_list.length, "Length of linked list (after first pop):")
+popped_node = linked_list.pop()
+check(1, popped_node.value, "Value of popped node (second pop):")
+check(None, linked_list.head, "Head of linked list (after second pop):")
+check(None, linked_list.tail, "Tail of linked list (after second pop):")
+check(0, linked_list.length, "Length of linked list (after second pop):")
+popped_node = linked_list.pop()
+check(None, popped_node, "Popped node from empty linked list (third pop):")
+check(None, linked_list.head, "Head of linked list (after third pop):")
+check(None, linked_list.tail, "Tail of linked list (after third pop):")
+check(0, linked_list.length, "Length of linked list (after third pop):")
+
 """

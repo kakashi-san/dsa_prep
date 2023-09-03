d9ece5d1546c4a02c29e1f29aab135b48ef3bce4
kakashi-san <siddheshpednekar94@gmail.com>
2023-09-03 19:50:58 +0530

    optimised prepend method in LinkedList class

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -133,14 +133,14 @@ class LinkedList:
         new_node = Node(value=value)
 
         if self.length == 0:
-            self.head = new_node
             self.tail = new_node
-            self.length += 1
 
         else:
             new_node.next = self.head
-            self.head = new_node
-            self.length += 1
+        
+        self.head = new_node
+        self.length += 1
+
 
         return True
         
